48de16a13470155f11caa77877090c93a8adcb4c
cyz912 <chenyzh@rdfz.cn>
2014-09-06 16:44:14 +0800

    2048 game

---
+++ 2048/js/game_manager.js
@@ -0,0 +1,88 @@
+function GameManager(size, InputManager, Actuator) {
+  this.size         = size;
+  this.inputManager = new InputManager;
+  this.actuator     = new Actuator;
+
+  this.running      = false;
+
+  this.inputManager.on("move", this.move.bind(this));
+  this.inputManager.on("restart", this.restart.bind(this));
+
+  this.inputManager.on('think', function() {
+    var best = this.ai.getBest();
+    this.actuator.showHint(best.move);
+  }.bind(this));
+
+
+  this.inputManager.on('run', function() {
+    if (this.running) {
+      this.running = false;
+      this.actuator.setRunButton('Auto-run');
+    } else {
+      this.running = true;
+      this.run()
+      this.actuator.setRunButton('Stop');
+    }
+  }.bind(this));
+
+  this.setup();
+}
+
+GameManager.prototype.restart = function () {
+  this.actuator.restart();
+  this.running = false;
+  this.actuator.setRunButton('Auto-run');
+  this.setup();
+};
+
+GameManager.prototype.setup = function () {
+  this.grid         = new Grid(this.size);
+  this.grid.addStartTiles();
+
+  this.ai           = new AI(this.grid);
+
+  this.score        = 0;
+  this.over         = false;
+  this.won          = false;
+
+  this.actuate();
+};
+
+GameManager.prototype.actuate = function () {
+  this.actuator.actuate(this.grid, {
+    score: this.score,
+    over:  this.over,
+    won:   this.won
+  });
+};
+
+GameManager.prototype.move = function(direction) {
+  var result = this.grid.move(direction);
+  this.score += result.score;
+
+  if (!result.won) {
+    if (result.moved) {
+      this.grid.computerMove();
+    }
+  } else {
+    this.won = true;
+  }
+
+  if (!this.grid.movesAvailable()) {
+    this.over = true;
+  }
+
+  this.actuate();
+}
+
+GameManager.prototype.run = function() {
+  var best = this.ai.getBest();
+  this.move(best.move);
+  var timeout = animationDelay;
+  if (this.running && !this.over && !this.won) {
+    var self = this;
+    setTimeout(function(){
+      self.run();
+    }, timeout);
+  }
+}
